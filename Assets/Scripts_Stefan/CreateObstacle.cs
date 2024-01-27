60cf952ab0d92b2a00a8fba8b9db9a6278f97010
Stefan Mitrevski <mitrevskistefan69@hotmail.com>
2024-01-28 00:52:14 +0100

    power ups

--- Assets/Scripts_Stefan/CreateObstacle.cs
+++ Assets/Scripts_Stefan/CreateObstacle.cs
@@ -7,6 +7,17 @@ public class CreateObstacle : MonoBehaviour
     public float wait = 0.5f;
     public GameObject Obstacle;
     public GameObject[] Platform = new GameObject[3];
+    public GameObject Invincibility;
+    public GameObject ExtraLife;
+    private float shouldSpawnPowerUp;
+
+    // if its 0.8 then we spawn invincibility
+    // otherwise we spawn an extra life
+    private float shouldSpawnInvincibility;
+
+    // limits the number of times the invincibility power up
+    // spawns for a run; currently its limited to only once per run
+    private int invincibilitySpawnCounter = 0;
 
     void Start()
     {
@@ -16,6 +27,19 @@ public class CreateObstacle : MonoBehaviour
     void createObstacle() {
         int row =  Random.Range(0,3);
         
-        Instantiate(Obstacle, new Vector3(10, Platform[row].transform.position.y, 0), Quaternion.identity);
+        shouldSpawnPowerUp = Random.Range(0.0f, 1f);
+        
+        if (shouldSpawnPowerUp >= 0.95f) {
+            shouldSpawnInvincibility = Random.Range(0.0f, 1f);
+
+            if (shouldSpawnInvincibility >= 0.85f && invincibilitySpawnCounter == 0) {
+                Instantiate(Invincibility, new Vector3(10, Platform[row].transform.position.y, 0), Quaternion.identity);
+                invincibilitySpawnCounter++;
+            } else {
+                Instantiate(ExtraLife, new Vector3(10, Platform[row].transform.position.y, 0), Quaternion.identity);
+            }
+        } else {
+            Instantiate(Obstacle, new Vector3(10, Platform[row].transform.position.y, 0), Quaternion.identity);
+        }
     }
 }
